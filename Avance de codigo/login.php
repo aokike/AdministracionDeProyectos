c0087b54eb8652cc456bccc2b6994a672da2785c
raulglezt <rglezt1490@gmail.com>
2015-05-08 12:13:06 -0500

    Avance del código
    Ventana de inicio de sesión

---
+++ Avance de codigo/login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tienda de Ropa</title>
+<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Raleway:400,200' rel='stylesheet' type='text/css'>
+<link href="css/estilo_login.css" rel="stylesheet">
+<link href="css/estilos_barra_social.css" rel="stylesheet">
+<link href="css/fonts.css" rel="stylesheet">
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body>
+<div class="container">
+	<div class="row login_box">
+	    <div class="col-md-12 col-xs-12" align="center">
+            
+            <div class="outter"><img src="img/descarga.jpg" class="image-circle"/></div>   
+            <h1 id="hora">Acceso al sistema</h1>            
+	    </div>
+        <div class="col-md-6 col-xs-6 follow line" align="center">
+
+        </div>
+               
+        <div class="col-md-12 col-xs-12 login_control">
+                <form action="#" method="post" accept-charset="utf-8"
+                        role="form"
+                        >
+                <div class="control">
+                    <div class="label">Usuario</div>
+                    <input  class="form-control" 
+                    placeholder="Usuario" name="login" id ="login" type="text"  required autofocustype="text"/>
+                </div>
+                
+                <div class="control">
+                     <div class="label">Contraseña</div>
+                    <input  class="form-control" 
+                    placeholder="Contraseña" name="password" id="password" type="password"  required  />
+                </div>
+                <div align="center">
+                     <button class="btn btn-orange" type="submit" name="submit">Acceso</button>
+                </div>
+                </form>  
+        </div>      
+    </div>
+</div>
+
+</body>
+</html>
